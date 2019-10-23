11a21e05b9fe4be42e276f3fb670b6c095359837
ASHWATHAMA <nawazuddinmorning@gmail.com>
2019-10-23 14:05:28 +0600

    added a new function called display

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello{
     public static void main (string[]args) {
-        system.out.println("vs code is awesome")
+        system.out.println("vs code is awesome");
+
+        greeting("Safwan");
+    }
+
+    public static void greeting(string name) {
+        system.out.println("welcome " + name );
     }
 }
